1eb4bc1065831ad74ebb0114501eae1f8114fe31
paulQR <paulqr10@gmail.com>
2019-07-24 14:54:32 +0200

    1 file modified

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -20,7 +20,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
   imports: [
