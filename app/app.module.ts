acbdea35793e06125c27b4b8db9e19da8fbc27e9
paulQR <paulqr10@gmail.com>
2019-07-24 11:23:01 +0200

    2 files modified, 6 added, 6 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage } from './pages/about/about';
+import { HomePage } from './pages/home/home';
+import { TabsPage } from './pages/tabs/tabs';
 
 @NgModule({
   declarations: [
