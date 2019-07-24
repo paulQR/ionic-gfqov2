3841bcb94f157d550ed5b198a4d73049a0724be6
paulQR <paulqr10@gmail.com>
2019-07-24 15:49:54 +0200

    1 file modified, 3 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,7 +6,7 @@ import { MyApp } from './app.component';
 import { Tab1Page } from './pages/tab1/tab1';
 import { Tab2Page } from './pages/tab2/tab2';
 import { TabsPage } from './pages/tabs/tabs';
-import { AgregarPage } from './pages/agregar/agregar.page';
+//import { AgregarPage } from './pages/agregar/agregar.page';
 
 // servicios
 import { DeseosService } from './services/deseos.service';
@@ -16,8 +16,7 @@ import { DeseosService } from './services/deseos.service';
     MyApp,
     Tab2Page,
     Tab1Page,
-    TabsPage,
-    AgregarPage
+    TabsPage 
   ],
   imports: [
     BrowserModule,
@@ -28,8 +27,7 @@ import { DeseosService } from './services/deseos.service';
     MyApp,
     Tab2Page,
     Tab1Page,
-    TabsPage,
-    AgregarPage
+    TabsPage
   ],
   providers: [DeseosService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
