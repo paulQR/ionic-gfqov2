6dc7ad7cfd9b6fcefcece961b25a2560e546d0cf
paulQR <paulqr10@gmail.com>
2019-07-24 10:07:18 +0200

    2 files modified, 2 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -12,7 +11,6 @@ import { TabsPage } from '../pages/tabs/tabs';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
@@ -24,7 +22,6 @@ import { TabsPage } from '../pages/tabs/tabs';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
