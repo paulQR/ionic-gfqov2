5b6f45436ce2aa99222310eea653a84e52e8b348
paulQR <paulqr10@gmail.com>
2019-07-24 14:57:26 +0200

    1 file modified

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -17,7 +17,7 @@ export const ROUTES: Routes = [
 
 */
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from  '@angular/router';
 
 const routes: Routes = [
 ];
